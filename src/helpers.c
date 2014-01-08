21ff04b38b606e54637096f5866acea055319e2d
George Griffin <george@passingleads.com>
2014-01-08 12:24:29 -0800

    asdf
    asdf

---
+++ src/helpers.c
@@ -0,0 +1,1 @@
+//screen dimentions in pixels
--- src/main.c
+++ src/main.c
@@ -1,15 +1,12 @@
 #include <pebble.h>
-
+	
 Window *my_window;
-TextLayer *text_layer;
-GContext *g_context;
-int32_t WIDTH  = 144;
-int32_t HEIGHT = 168;
 int32_t HOUR_ANGLE;
 int32_t MINUTE_ANGLE;
-
-//GPoint CENTER_WATCH = GPoint(WIDTH/2, HEIGHT/2);
-//GPoint CENTER_APP   = GPoint(WIDTH/2, (HEIGHT/2)+8); //Translate down 8px. Find out of this is right.
+int32_t SECOND_ANGLE;
+int WIDTH  = 144;
+int HEIGHT = 168;
+//int STATBAR= 16;
 
 GPoint get_endpoint(GPoint center, int length, int32_t angle){
 	int32_t y = (-cos_lookup(angle) * length / TRIG_MAX_RATIO) + center.y;
@@ -18,46 +15,54 @@ GPoint get_endpoint(GPoint center, int length, int32_t angle){
 }
 
 void draw_circle(Layer *layer, GContext *gctxt){
-	GPoint circ_center = GPoint(WIDTH/2, (HEIGHT/2)+12);
-	//Draw hour circle
-	graphics_context_set_fill_color(gctxt, GColorBlack);
-	graphics_fill_circle(gctxt, circ_center, (WIDTH/2)-8);
-	//Draw minute circle
+	GPoint circ_center = GPoint(WIDTH/2, (HEIGHT/2));
+    //Emblacken the screen
+	graphics_context_set_fill_color(gctxt, GColorBlack); graphics_fill_rect(gctxt, GRect(0,0,WIDTH,HEIGHT), 0, GCornerNone); 
+	//Draw outer circle 
 	graphics_context_set_fill_color(gctxt, GColorWhite);
-	graphics_fill_circle(gctxt, circ_center, (WIDTH/6));
+	graphics_fill_circle(gctxt, circ_center, (WIDTH/2)-8);
+	//Draw inner circle
+	graphics_context_set_fill_color(gctxt, GColorBlack);
+	graphics_fill_circle(gctxt, circ_center, (WIDTH/4));
+	//Draw hour line
+	graphics_context_set_stroke_color(gctxt, GColorBlack);
+    graphics_draw_line(gctxt, get_endpoint(circ_center, (WIDTH/4), MINUTE_ANGLE), get_endpoint(circ_center, (WIDTH/2), MINUTE_ANGLE));
+    graphics_draw_line(gctxt, get_endpoint(circ_center, (WIDTH/4), SECOND_ANGLE), get_endpoint(circ_center, (WIDTH/2), SECOND_ANGLE));
+	
 	
 	graphics_context_set_stroke_color(gctxt, GColorWhite);
-	graphics_draw_line(gctxt, circ_center, get_endpoint(circ_center, (WIDTH/2)-8, HOUR_ANGLE));
-
-	graphics_context_set_stroke_color(gctxt, GColorBlack);
-	graphics_draw_line(gctxt, circ_center, get_endpoint(circ_center, (WIDTH/2)-8, MINUTE_ANGLE));
+	graphics_draw_line(gctxt, circ_center, get_endpoint(circ_center, (WIDTH/4), HOUR_ANGLE));
+	
+	//graphics_context_set_stroke_color(gctxt, GColorBlack);
+	//int32_t angle = (TRIG_MAX_ANGLE * 4 / 12);
+	//GPoint start;
+	//GPoint end;
+	//int32_t c = 0;
+	//while(c!=12){
+	//	angle = (TRIG_MAX_ANGLE * c / 12);	
+	//   start = get_endpoint(circ_center, (WIDTH/6)+33, angle);
+	//    end = get_endpoint(circ_center, (WIDTH/2), angle);
+		//graphics_draw_line(gctxt, start, end);
+	//	c = c + 3;
+	//}
 }
 
 void tick_handler(struct tm *tick_time, TimeUnits units_changed){
-	//strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
 	struct tm tm = *tick_time;
+	SECOND_ANGLE = TRIG_MAX_ANGLE * tm.tm_sec / 60;
     MINUTE_ANGLE = TRIG_MAX_ANGLE * tm.tm_min / 60;
-	HOUR_ANGLE = TRIG_MAX_ANGLE * tm.tm_hour / 12;
+	HOUR_ANGLE = (TRIG_MAX_ANGLE * tm.tm_hour / 12) + (((TRIG_MAX_ANGLE/12)/60) * tm.tm_min);
 	layer_mark_dirty(window_get_root_layer(my_window));
 
 }
 
 void window_load(Window *window){
-	text_layer = text_layer_create(GRect(0, 0, 144, 20));
-	text_layer_set_text(text_layer, "Even you will die.");
-	text_layer_set_background_color(text_layer, GColorClear);
-	text_layer_set_text_color(text_layer, GColorBlack);
-	//text_layer_set_size(text_layer, GSize(5));
-	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-	layer_add_child(window_get_root_layer(my_window), (Layer*) text_layer);
     layer_set_update_proc(window_get_root_layer(my_window), draw_circle);
-
-	//grahics_context_set_stroke_color(g_context, GColorBlack);
-	
 }
 
 void window_unload(Window *window){
-	text_layer_destroy(text_layer);
+	//text_layer_destroy(text_layer);
+	//layer_destroy(Layer* layer)
 }
                                                              
 void handle_init(void) {
